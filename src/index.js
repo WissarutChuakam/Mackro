c6a897bbdee3b4f5ef4ac3f6d30073a9475bfd25
Wissarut_Chuakam <wissarut.ch@rmuti.ac.th>
2024-01-28 21:51:54 +0700

    product

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const cors = require("cors")
 const app = express();
 
 const authRoute =require("./routers/auth-route")
-const productRoute = require("./routers/product-route")
+const productRoute = require("./routers/product-route");
+const errorHandler = require('./middlewares/error');
+const notFoundHandler = require('./middlewares/not-found')
 
 app.use(cors())
 app.use(express.json())
@@ -12,8 +14,8 @@ app.use(express.json())
 app.use("/auth", authRoute)
 app.use("/product", productRoute)
 
-// app.use(errorHandler);
-// app.use("*", notFoundHandler)
+app.use(errorHandler);
+app.use("*", notFoundHandler)
 
 const port = process.env.POST
 app.listen(port,()=>{
